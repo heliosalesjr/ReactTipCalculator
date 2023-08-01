b3d006446be0b4f8d92ce9761b5edb77953514c1
heliosalesjr <heliosalesjr@gmail.com>
2023-08-01 14:25:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+import TipCalculator from "./components/TipCalculator";
+
+export default function App() {
+  return (
+    <div>
+      <TipCalculator />
+    </div>
+  );
+}
+
